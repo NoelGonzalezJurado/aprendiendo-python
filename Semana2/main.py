d19c8aeac83d3a6e48fb953d1495340c564512b8
Noel Gonzalez Jurado <ngonzalezjurado5@gmail.com>
2026-05-20 13:16:59 +0200

    query params en FastAPI

--- Semana2/main.py
+++ Semana2/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+def inicio():
+    return {"mensaje": "Hola desde mi primera API"}
+
 @app.get("/saludo/{nombre}")
 def saludar(nombre: str):
     return {"mensaje": f"Hola {nombre}, bienvenido a mi API"}
@@ -11,6 +15,15 @@ def clima(ciudad: str):
     import requests
     respuesta = requests.get(f"https://wttr.in/{ciudad}?format=j1")
     datos = respuesta.json()
+
     temp = datos["current_condition"][0]["temp_C"]
     desc = datos["current_condition"][0]["weatherDesc"][0]["value"]
     return {"ciudad": ciudad, "temperatura": temp, "estado": desc}
+
+@app.get("/buscar")
+def buscar(ciudad: str, limite: int = 3):
+    return {
+        "ciudad": ciudad,
+        "limite": limite,
+        "mensaje": f"Buscando {limite} resultados para {ciudad}"
+    }
